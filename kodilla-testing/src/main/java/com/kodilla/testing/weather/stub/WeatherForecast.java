3f3941c7711d5007eb6d1b50269e52348b3fa341
iskic650 <i.alimbajew@gmail.com>
2023-07-31 23:44:06 +0200

    6.5 - Zadanie: Rozbudowa WeatherForecastTestSuite (z błędem w metodzie medianTemperature()- nie wiem na czym polega błąd).

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -0,0 +1,43 @@
+package com.kodilla.testing.weather.stub;
+
+import java.util.*;
+
+public class WeatherForecast {
+    private Temperatures temperatures;
+
+    public WeatherForecast(Temperatures temperatures) {
+        this.temperatures = temperatures;
+    }
+
+    public Map<String, Double> calculateForecast() {
+        Map<String, Double> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> temperature: temperatures.getTemperatures().entrySet()) {
+            resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
+        }
+        return resultMap;
+    }
+    public double avarageTemperature(){
+        Map<String, Double> temperatures1 = temperatures.getTemperatures();
+        double result = 0.0;
+        for (Double temp: temperatures1.values()) {
+            result += temp;
+        }
+        result = result/temperatures1.size();
+        return result;
+    }
+
+    public double medianTemperature(){
+        double result;
+        ArrayList<Double> temperatureList = new ArrayList<>();
+        for(Double t: temperatures.getTemperatures().values()){
+            temperatureList.add(t);
+        }
+        Collections.sort(temperatureList);
+        if(temperatureList.size()%2 == 1){
+            result = temperatureList.get(temperatureList.size()/2);
+        }else result = (temperatureList.get(temperatureList.size() / 2)
+                + temperatureList.get(temperatureList.size() / 2 + 1)) / 2;
+        return result;
+    }
+}
