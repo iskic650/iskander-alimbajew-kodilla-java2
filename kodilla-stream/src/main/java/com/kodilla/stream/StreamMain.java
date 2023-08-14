1805f7019206010b80b280095dc959a4a07479b1
iskic650 <i.alimbajew@gmail.com>
2023-08-14 12:49:30 +0200

    7.3. (poprawka)

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.Year;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,9 +11,10 @@ public class StreamMain {
 
     public static void main(String[] args) {
         Forum forum = new Forum();
+        int year = Year.now().getValue();
         Map<Integer, ForumUser> theResultList = forum.getList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getYearOfBirth() < 2003)
+                .filter(forumUser -> forumUser.getYearOfBirth() < year - 20)
                 .filter(forumUser -> forumUser.getPostQuantity() > 0)
                 .collect(Collectors.toMap(ForumUser::getIndex, forumUser -> forumUser));
 
