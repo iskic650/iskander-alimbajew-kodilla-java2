bde49dc28db1f658a124e039e86b7b6b96c4eb8f
iskic650 <i.alimbajew@gmail.com>
2023-09-09 12:24:04 +0200

    9.1 Zadanie: funkcyjna iteracja po tytułach

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/KodillaGoodPatternsApplication.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGoodPatternsApplication {
+
+	public static void main(String[] args) {
+		MovieStore movieStore = new MovieStore();
+		movieStore.getMovies().values().stream().forEach(m -> System.out.print(m + "!"));
+
+		//Map<String, List<String>> movieList = Stream.iterate();
+	}
+}
