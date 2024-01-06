e435cd9ed29e64d37557d5c410186191bac65d63
iskic650 <i.alimbajew@gmail.com>
2024-01-06 23:49:51 +0100

    11.2. Prototype

--- kodilla-good-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-good-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -6,24 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class LibraryTestSuite {
-
-    /*@BeforeEach
-    void given() {
-        Library library = new Library("library");
-
-        Book book1 = new Book("T1", "A1", LocalDate.of(2001,1,1));
-        Book book2 = new Book("T2", "A2", LocalDate.of(2002,1,1));
-        Book book3 = new Book("T3", "A3", LocalDate.of(2003,1,1));
-        Book book4 = new Book("T4", "A4", LocalDate.of(2004,1,1));
-
-        library.books.add(book1);
-        library.books.add(book2);
-        library.books.add(book3);
-        library.books.add(book4);
-    }*/
 
     @Test
     void testLibraryShallowCopy() {
@@ -50,7 +34,7 @@ public class LibraryTestSuite {
         }
 
         //Then
-        assertNotEquals(library.books, clonedLibrary.books);
+        assertEquals(library.books, clonedLibrary.books);
     }
 
     @Test
